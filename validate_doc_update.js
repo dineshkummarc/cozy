e9b22dd3b593b1bda163be9379f78938acbceee9
gurdiga <gurdiga@gmail.com>
2009-08-14 13:31:57 +0300

    Added validation function

---
+++ validate_doc_update.js
@@ -0,0 +1,6 @@
+function(newDoc, oldDoc, userCtx) {
+  // http://books.couchdb.org/relax/design-documents/validation-functions
+  // use !code?
+  // throw({forbidden : 'no way'});
+  // throw({unauthorized : message});
+}

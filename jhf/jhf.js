7805068d967240ce75043d3b6b6a705e0315f39a
gurdiga <gurdiga@gmail.com>
2009-10-10 08:30:15 +0300

    New formBuilder
    More to come.

--- jhf/jhf.js
+++ jhf/jhf.js
@@ -1,14 +1,124 @@
 // How to name organize this?
 var JHF = {
   buildField: function(id, metaData) {
-    var label = metaData.label || id;
+    var type = metaData.type || "text";
+    var builder = this.builders[type + "Field"];
 
-    return {li: [
-      {label: label, attributes: {for: id}},
-      {input: null, attributes: {
+    if (!builder) throw "JHF.buildField: no builder for type [" + type + "]";
+
+    return {li: builder(id, metaData)};
+  },
+
+  builders: {
+    textField: function(id, metaData) {
+      var attributes = {
         id: id,
         type: "text"
-      }}
-    ]};
+      };
+
+      if (metaData.disabled) attributes.disabled = "disabled";
+      if (metaData.readonly) attributes.readonly = "readonly";
+      if (metaData.default) attributes.value = metaData.default;
+
+      return [
+        {label: metaData.label || id, attributes: {for: id}},
+        {input: null, attributes: attributes}
+      ];
+    },
+
+    passwordField: function(id, metaData) {
+      var attributes = {
+        id: id,
+        type: "password"
+      };
+
+      if (metaData.disabled) attributes.disabled = "disabled";
+
+      return [
+        {label: metaData.label || id, attributes: {for: id}},
+        {input: null, attributes: attributes}
+      ];
+    },
+
+    radioField: function(id, metaData) {
+      var ul = [];
+
+      for (var option in metaData.options) {
+        var disabled = false;
+        var label = metaData.options[option];
+
+        if (/^-/.test(option)) {
+          disabled = true;
+          option = option.substr(1);
+        }
+
+        var attributes = {
+          id: id + "_" + option,
+          name: id,
+          type: "radio",
+          value: option
+        };
+
+        if (disabled) attributes.disabled = "disabled";
+        if (metaData.default && metaData.default == option) attributes.checked = "checked";
+
+        ul.push({li: [
+          {input: null, attributes: attributes},
+          {label: label, attributes: {for: id + "_" + option}}
+        ]});
+      }
+
+      return [
+        {label: metaData.label || id},
+        {ul: ul}
+      ];
+    },
+
+    checkboxField: function(id, metaData) {
+      var attributes = {
+        id: id,
+        type: "checkbox"
+      };
+
+      if (metaData.disabled) attributes.disabled = "disabled";
+
+      return [
+        {input: null, attributes: attributes},
+        {label: metaData.label || id, attributes: {for: id}}
+      ];
+    },
+
+    selectField: function(id, metaData) {
+      var select = [];
+
+      for (var option in metaData.options) {
+        var optionLabel = metaData.options[option];
+        var disabled = false;
+
+        if (/^-/.test(option)) {
+          disabled = true;
+          option = option.substr(1);
+        }
+
+        var attributes = {value: option};
+
+        if (disabled) attributes.disabled = "disabled";
+
+        select.push({
+          option: optionLabel,
+          attributes: attributes
+        });
+      }
+
+      var attributes = {id: id};
+
+      if (metaData.multiple) attributes.multiple = "multiple";
+      if (metaData.disabled) attributes.disabled = "disabled";
+
+      return [
+        {label: metaData.label || id, attributes: {for: id}},
+        {select: select, attributes: attributes}
+      ];
+    }
   }
 };
--- jhf/tests.js
+++ jhf/tests.js
@@ -6,7 +6,7 @@ load(
   "lib/test_runner/TestRunner.js"
 );
 
-print("Testing JHF:");
+print("Testing JHF.buildField:");
 
 /*
  * Add a test object to the tests array.
@@ -21,7 +21,7 @@ TestRunner.testDefaults = {
 };
 
 var tests = [{
-  description: "JHF.buildField(): text input with a label",
+  description: "JHF.buildField: text input with a label",
   arguments: ["name", {
     label: "Name:"
   }],
@@ -31,6 +31,285 @@ var tests = [{
       id: "name",
       type: "text"
     }}
+  ]})
+}, {
+  description: "JHF.buildField: disabled text input",
+  arguments: ["name", {
+    label: "Name:",
+    disabled: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Name:", attributes: {for: "name"}},
+    {input: null, attributes: {
+      id: "name",
+      type: "text",
+      disabled: "disabled"
+    }}
+  ]})
+}, {
+  description: "JHF.buildField: read only text input",
+  arguments: ["name", {
+    label: "Name:",
+    readonly: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Name:", attributes: {for: "name"}},
+    {input: null, attributes: {
+      id: "name",
+      type: "text",
+      readonly: "readonly"
+    }}
+  ]})
+}, {
+  description: "JHF.buildField: text input with a default value",
+  arguments: ["name", {
+    label: "Name:",
+    default: "JSON"
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Name:", attributes: {for: "name"}},
+    {input: null, attributes: {
+      id: "name",
+      type: "text",
+      value: "JSON"
+    }}
+  ]})
+}, {
+  description: "JHF.buildField: password input with a label",
+  arguments: ["password", {
+    label: "Password:",
+    type: "password"
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Password:", attributes: {for: "password"}},
+    {input: null, attributes: {
+      id: "password",
+      type: "password"
+    }}
+  ]})
+}, {
+  description: "JHF.buildField: disabled password input",
+  arguments: ["password", {
+    label: "Password:",
+    type: "password",
+    disabled: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Password:", attributes: {for: "password"}},
+    {input: null, attributes: {
+      id: "password",
+      type: "password",
+      disabled: "disabled"
+    }}
+  ]})
+}, {
+  description: "JHF.buildField: radio with a default option selected",
+  arguments: ["age", {
+    label: "Age:",
+    type: "radio",
+    options: {
+      teenager: "12-17 years",
+      mature: "18-25 years"
+    }, 
+    default: "teenager"
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Age:"},
+    {ul: [
+      {li: [
+        {input: null, attributes: {
+          id: "age_teenager",
+          name: "age",
+          type: "radio",
+          value: "teenager",
+          checked: "checked"
+        }},
+        {label: "12-17 years", attributes: {for: "age_teenager"}},
+      ]},
+      {li: [
+        {input: null, attributes: {
+          id: "age_mature",
+          name: "age",
+          type: "radio",
+          value: "mature"
+        }},
+        {label: "18-25 years", attributes: {for: "age_mature"}},
+      ]}
+    ]}
+  ]})
+}, {
+  description: "JHF.buildField: radio",
+  arguments: ["age", {
+    label: "Age:",
+    type: "radio",
+    options: {
+      teenager: "12-17 years",
+      mature: "18-25 years"
+    }
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Age:"},
+    {ul: [
+      {li: [
+        {input: null, attributes: {
+          id: "age_teenager",
+          name: "age",
+          type: "radio",
+          value: "teenager"
+        }},
+        {label: "12-17 years", attributes: {for: "age_teenager"}},
+      ]},
+      {li: [
+        {input: null, attributes: {
+          id: "age_mature",
+          name: "age",
+          type: "radio",
+          value: "mature"
+        }},
+        {label: "18-25 years", attributes: {for: "age_mature"}},
+      ]}
+    ]}
+  ]})
+}, {
+  description: "JHF.buildField: radio with a disabled option",
+  arguments: ["age", {
+    label: "Age:",
+    type: "radio",
+    options: {
+      "-teenager": "12-17 years",
+      mature: "18-25 years"
+    }
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Age:"},
+    {ul: [
+      {li: [
+        {input: null, attributes: {
+          id: "age_teenager",
+          name: "age",
+          type: "radio",
+          value: "teenager",
+          disabled: "disabled"
+        }},
+        {label: "12-17 years", attributes: {for: "age_teenager"}},
+      ]},
+      {li: [
+        {input: null, attributes: {
+          id: "age_mature",
+          name: "age",
+          type: "radio",
+          value: "mature"
+        }},
+        {label: "18-25 years", attributes: {for: "age_mature"}},
+      ]}
+    ]}
+  ]})
+}, {
+  description: "JHF.buildField: checkbox",
+  arguments: ["accept", {
+    label: "Accept usage terms",
+    type: "checkbox"
+  }],
+  expected: JSON.stringify({li: [
+    {input: null, attributes: {
+      id: "accept",
+      type: "checkbox"
+    }},
+    {label: "Accept usage terms", attributes: {for: "accept"}}
+  ]})
+}, {
+  description: "JHF.buildField: disabled checkbox",
+  arguments: ["accept", {
+    label: "Accept usage terms",
+    type: "checkbox",
+    disabled: true
+  }],
+  expected: JSON.stringify({li: [
+    {input: null, attributes: {
+      id: "accept",
+      type: "checkbox",
+      disabled: "disabled"
+    }},
+    {label: "Accept usage terms", attributes: {for: "accept"}}
+  ]})
+}, {
+  description: "JHF.buildField: select",
+  arguments: ["level", {
+    label: "Level:",
+    type: "select",
+    options: {
+      "1": "low",
+      "2": "middle",
+      "3": "high"
+    }
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Level:", attributes: {for: "level"}},
+    {select: [
+      {option: "low", attributes: {value: "1"}},
+      {option: "middle", attributes: {value: "2"}},
+      {option: "high", attributes: {value: "3"}}
+    ], attributes: {id: "level"}}
+  ]})
+}, {
+  description: "JHF.buildField: disabled select",
+  arguments: ["level", {
+    label: "Level:",
+    type: "select",
+    options: {
+      "1": "low",
+      "2": "middle",
+      "3": "high"
+    },
+    disabled: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Level:", attributes: {for: "level"}},
+    {select: [
+      {option: "low", attributes: {value: "1"}},
+      {option: "middle", attributes: {value: "2"}},
+      {option: "high", attributes: {value: "3"}}
+    ], attributes: {id: "level", disabled: "disabled"}}
+  ]})
+}, {
+  description: "JHF.buildField: select with a disabled option",
+  arguments: ["level", {
+    label: "Level:",
+    type: "select",
+    options: {
+      "1": "low",
+      "-2": "middle",
+      "3": "high"
+    },
+    disabled: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Level:", attributes: {for: "level"}},
+    {select: [
+      {option: "low", attributes: {value: "1"}},
+      {option: "middle", attributes: {value: "2", disabled: "disabled"}},
+      {option: "high", attributes: {value: "3"}}
+    ], attributes: {id: "level", disabled: "disabled"}}
+  ]})
+}, {
+  description: "JHF.buildField: select multiple",
+  arguments: ["levels", {
+    label: "Levels:",
+    type: "select",
+    options: {
+      "1": "low",
+      "2": "middle",
+      "3": "high"
+    },
+    multiple: true
+  }],
+  expected: JSON.stringify({li: [
+    {label: "Levels:", attributes: {for: "levels"}},
+    {select: [
+      {option: "low", attributes: {value: "1"}},
+      {option: "middle", attributes: {value: "2"}},
+      {option: "high", attributes: {value: "3"}}
+    ], attributes: {id: "levels", multiple: "multiple"}}
   ]})
 }];
 

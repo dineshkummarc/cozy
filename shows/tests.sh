670500cf8e34e6de4da7d2041001abda5138b596
gurdiga <gurdiga@gmail.com>
2009-10-14 03:44:01 +0300

    Refactored shows' shecker

--- shows/tests.sh
+++ shows/tests.sh
@@ -1,18 +1,26 @@
 #!/bin/sh -e
 
-echo "Testing shows:"
+echo "Testing if shows compile:"
 
 find ./shows \
   -maxdepth 1 \
-  ! -path "./shows/tests.sh" \
   -name "*.js" \
 | while read file; do
   echo -n "- $file: "
 
+  logfile="/tmp/`basename $file`.log"
+
   cat shows/tests/show_prefix.js $file shows/tests/show_suffix.js | \
     sed -E -e 's|// +\!code +(.*)$|load\("\1"\)|' | \
-    /usr/local/bin/js -w -s
+    /usr/local/bin/js -s > $logfile 2>&1
 
+  if [ -s "$logfile" ]; then
+    cat $logfile
+    rm $logfile
+    exit 1
+  fi
+
+  rm -f $logfile
   echo "OK."
 done
 
